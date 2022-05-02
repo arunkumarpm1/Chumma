686787fdc6b0a6e2c93ca7a9117b4919f2b119e9
arunkumarpm1 <arunhugh17@gmail.com>
2022-05-02 14:02:43 +0530

    hi

---
+++ src/org/sample/Gitb.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Gitb {
+	
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b= 20;
+		
+		String s = new String(); 
+		s.chars();
+		
+		System.out.println(a+b);
+		
+	}
+
+}
